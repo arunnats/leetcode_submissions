a15a4deca6acb060bfd9438d4fef6a56af543d60
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-12 15:20:57 +0530

    Time: 11 ms (99.33%), Space: 30 MB (21.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,13 +1,20 @@
+const int ZERO = [](){
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+return 0;
+}();
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int start = 0, end = nums.size()-1, mid;
-        while(start <= end)
+    int search(vector<int>& nums, int target) 
+    {
+        int left = 0, right = nums.size();
+
+        while (left < right)
         {
-            mid = (start + end)/2;
-            if(nums[mid] > target) end = mid - 1;
-            else if(nums[mid] < target) start = mid+1;
-            else if(nums[mid] == target) return mid;
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) right = mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else return mid;
         }
         return -1;
     }
