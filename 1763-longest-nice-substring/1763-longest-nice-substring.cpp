77b7d0f61ecc5a8c3f88285820c62a10c2d0f1db
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-14 10:55:29 +0530

    Time: 3 ms (94.08%), Space: 7.4 MB (96.17%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestNiceSubstring(string s) 
+    {
+        int n = s.size();
+        int maxLen = 0;
+        int startIndex = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int lowerCase=0;
+            int upperCase=0;
+            for(int j = i; j < n; j++)
+            {
+                if( s[j] >= 'a')
+                    lowerCase = lowerCase | ( 1<<( s[j]-'a' ) );
+                else
+                    upperCase = upperCase | ( 1<< (s[j]-'A' ) );
+
+                if ((lowerCase^upperCase)==0)
+                {
+                    if (maxLen<j-i+1)
+                    {
+                        startIndex=i;
+                        maxLen=j-i+1;
+                    }
+                }
+
+            }
+        }
+        if (maxLen==0)
+            return "";
+        return s.substr(startIndex,maxLen);
+    }
+};
