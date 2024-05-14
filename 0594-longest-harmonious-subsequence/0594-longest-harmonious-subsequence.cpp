1be0882072e24dff7576af5a0eb52852eadfa3a6
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-14 13:00:02 +0530

    Time: 38 ms (89.49%), Space: 35.9 MB (93.38%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -2,31 +2,50 @@ class Solution {
 public:
     int findLHS(vector<int>& nums) 
     {
-        map<int, int> freqMap;
-        set<int> numList;
+        // map<int, int> freqMap;
+        // set<int> numList;
 
-        for(const auto& num : nums)
-        {
-            freqMap[num]++;
-            numList.insert(num);
-        }
+        // for(const auto& num : nums)
+        // {
+        //     freqMap[num]++;
+        //     numList.insert(num);
+        // }
         
+        // int LHS = 0;
+        // for(const auto& num: numList)
+        // {
+        //     int Ifreq = 0;
+        //     int I1freq = 0;
+
+        //     if(freqMap.count(num) > 0 && freqMap.count(num-1) > 0)
+        //     {
+        //         Ifreq = freqMap[num];
+        //         I1freq = freqMap[num-1];
+        //     }
+
+        //     LHS = max( LHS, Ifreq + I1freq );
+        // }
+
+        // return LHS;
+
+        sort(nums.begin(), nums.end());
+
+        int l = 0; 
+        int n  = nums.size();
         int LHS = 0;
-        for(const auto& num: numList)
+
+        for(int r = 0; r < n; ++r)
         {
-            int Ifreq = 0;
-            int I1freq = 0;
-
-            if(freqMap.count(num) > 0 && freqMap.count(num-1) > 0)
+            while (nums[r] - nums[l] > 1 )
             {
-                Ifreq = freqMap[num];
-                I1freq = freqMap[num-1];
+                l++;
             }
-
-            LHS = max( LHS, Ifreq + I1freq );
+            if(nums[r] - nums[l] == 1)
+            {
+                LHS = max(LHS, r - l + 1);
+            }
+         
         }
-
         return LHS;
-
     }
 };
