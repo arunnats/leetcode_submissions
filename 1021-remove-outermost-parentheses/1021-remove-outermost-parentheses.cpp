d36531733ce4c8659981c897308a4898139b0c8f
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-18 15:41:34 +0530

    Time: 27 ms (5%), Space: 177.7 MB (5.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,52 +2,75 @@ class Solution {
 public:
     string removeOuterParentheses(string s)
     {
-        string ans = "";
-        stack <char> stackOne;
-        stack <char> stackTwo;
-        int l = 0;
-        int r = 0;
-        int n = s.size();
+        // string ans = "";
+        // stack <char> stackOne;
+        // stack <char> stackTwo;
+        // int l = 0;
+        // int r = 0;
+        // int n = s.size();
 
-        for(int i = 0; i < n; i++)
+        // for(int i = 0; i < n; i++)
+        // {
+        //    char ch = s[i];
+
+        //     if( ch == '(' )
+        //     {
+        //         l++;
+        //         stackOne.push(ch);
+        //     }
+        //     else
+        //     {
+        //         r++;
+        //         stackOne.push(ch);
+        //     }
+
+        //     if(l == r)
+        //     {
+        //         stackOne.pop();
+        //         r--;
+
+        //         for(int i = 0; i < (l+r); i++)
+        //         {
+        //             stackTwo.push(stackOne.top());
+        //             stackOne.pop();
+        //         }
+
+        //         l = 0;
+        //         r = 0;
+
+        //         stackTwo.pop();
+                
+        //         while(!stackTwo.empty())
+        //         {
+        //             ans = ans + stackTwo.top();
+        //             stackTwo.pop();
+        //         }
+        //     }
+        // }
+
+        // return ans;   
+
+        string ans;
+        int count = 0;
+
+        for(const auto& ch : s)
         {
-           char ch = s[i];
-
-            if( ch == '(' )
+            if(ch == '(')
             {
-                l++;
-                stackOne.push(ch);
+                if(count > 0)
+                    ans = ans + ch;
+                count++;
             }
             else
             {
-                r++;
-                stackOne.push(ch);
-            }
-
-            if(l == r)
-            {
-                stackOne.pop();
-                r--;
-
-                for(int i = 0; i < (l+r); i++)
+                count--;
+                if(count > 0)
                 {
-                    stackTwo.push(stackOne.top());
-                    stackOne.pop();
-                }
-
-                l = 0;
-                r = 0;
-
-                stackTwo.pop();
-                
-                while(!stackTwo.empty())
-                {
-                    ans = ans + stackTwo.top();
-                    stackTwo.pop();
+                    ans = ans + ch;
                 }
             }
         }
 
-        return ans;   
+        return ans;
     }
 };
