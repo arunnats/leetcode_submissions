02a3b46b5fda3166ba867c72bccc5ce2e7454b6d
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-18 22:41:56 +0530

    Time: 0 ms (100%), Space: 7.2 MB (13.23%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) 
+    {
+        std::vector<int> num;
+        int temp = n;
+        while(temp > 0)
+        {
+            num.push_back(temp % 10);
+            temp = temp / 10;
+        }    
+        std::reverse(num.begin(), num.end()); 
+
+        int size = num.size();
+        int i = size - 2;
+
+        while(i >= 0 && num[i] >= num[i + 1])
+        {
+            i--;
+        }
+
+        if (i >= 0)
+        {
+            int j = size - 1;
+            while (j >= 0 && num[j] <= num[i])
+            {
+                j--;
+            }
+            std::swap(num[i], num[j]);
+        }
+        std::reverse(num.begin() + i + 1, num.end());
+
+        long long ans = 0;
+        for (int digit : num)
+        {
+            ans = ans * 10 + digit;
+        }
+
+        if (ans > INT_MAX || ans <= n)
+            return -1;
+        else 
+            return static_cast<int>(ans);
+    }
+};
