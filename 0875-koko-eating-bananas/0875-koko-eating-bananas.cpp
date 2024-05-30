0b28db18b51888345e09059300862bde6dbe44fa
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-05-31 00:13:56 +0530

    Time: 20 ms (96.89%), Space: 21.5 MB (29.7%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -7,30 +7,40 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) 
     {
-        int l = 1;
-        int r = *max_element(piles.begin(), piles.end());
+        int maxPile = 0;
 
-        while (l < r) 
+        for(const auto& num: piles)
+            maxPile = max(maxPile, num);
+
+        long long l = 1;
+        long long r = maxPile;
+        long long ans = maxPile;
+
+        while(l <= r)
         {
-            int mid = l + (r - l) / 2;
-            if(canFinish(piles, mid, h))
-                r = mid;
+            long long mid = l + (r - l) / 2;
+
+            if(findK(piles, mid) <= h)
+            {
+                ans = mid;
+                r = mid - 1;
+            }
             else
                 l = mid + 1;
         }
-        return l;
+
+        return static_cast<int>(ans);
     }
-    
-    bool canFinish(vector<int>& piles, int k, int h)
-    { 
-        int hours = 0;
-        for(const auto& pile : piles)
+
+    long long findK(const vector<int>& piles, long long k)
+    {
+        long long totalTime = 0;
+
+        for(int i = 0; i < piles.size(); i++)
         {
-            int x = pile / k;
-            hours = hours + x;
-            if(pile % k != 0)
-                hours++;
+            totalTime += (piles[i] + k - 1) / k;
         }
-        return hours <= h;
-    }
+
+        return totalTime;
+    } 
 };
