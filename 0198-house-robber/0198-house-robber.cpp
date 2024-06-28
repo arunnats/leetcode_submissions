81453ab51ef8ebf61e644830930fa35f4014045f
Arun Natarajan <118368673+arunnats@users.noreply.github.com>
2024-06-29 00:27:11 +0530

    Time: 4 ms (31.49%), Space: 9.4 MB (10.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,47 +3,20 @@ public:
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
-        if(n == 1)
-            return nums[0];
-        if(n == 2)
-            return max(nums[0], nums[1]);
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
 
-        vector<int> dp(n);
+        vector<int> dp(n, 0);
 
         dp[0] = nums[0];
-        dp[1] = nums[1];
-
-        int idx_max = (dp[0] > dp[1]) ? 0 : 1; 
-        int idx_nextmax = 1 - idx_max;
+        dp[1] = max(nums[0], nums[1]);
 
         for(int i = 2; i < n; i++)
         {
-            if(idx_max != i-1)
-            {
-                dp[i] = nums[i] + dp[idx_max];
-                if(dp[i] > dp[idx_max])
-                {
-                    idx_nextmax = idx_max;
-                    idx_max = i;
-                }
-            }
-            else
-            {
-                dp[i] = nums[i] + dp[idx_nextmax];
-                if(dp[i] > dp[idx_max])
-                {
-                    idx_nextmax = idx_max;
-                    idx_max = i;
-                }
-                else if(dp[i] > dp[idx_nextmax]) 
-                {
-                    idx_nextmax = i; 
-                }
-            }
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
         }
 
-        for(auto num: dp)
-            cout << num <<" ";
-        return dp[idx_max];
+        return dp[n-1];
     }
 };
